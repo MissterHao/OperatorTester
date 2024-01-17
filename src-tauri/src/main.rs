453a0b29cba7bdfd23ad7e102fdd30d0c10bac7a
MissterHao <henryliking@gmail.com>
2024-01-17 09:28:23 +0800

    feat: register multiple commands to tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,29 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+pub mod domain;
+pub mod application;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn yoooo(name: &str) -> String {
+    format!("YOOOOOOO!! I am {}!", name)
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            yoooo,
+            application::commands::action_basic_information::xddddd,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -7,7 +7,10 @@ const name = ref("");
 
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsg.value = await invoke("greet", { name: name.value });
+  greetMsg.value = 
+      await invoke("greet", { name: name.value }) + "<br>" + 
+      await invoke("yoooo", { name: name.value }) + "<br>"  + 
+      await invoke("xddddd", { name: name.value });
 }
 </script>
 
