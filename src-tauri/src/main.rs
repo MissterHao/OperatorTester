dcc3075c00b1eb3e7cfcc24f153fde8bc71e3832
MissterHao <henryliking@gmail.com>
2024-01-18 14:15:06 +0800

    feat: add transprant background

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+use window_vibrancy::{apply_blur, apply_vibrancy, NSVisualEffectMaterial};
+
 pub mod domain;
 pub mod application;
 
@@ -24,6 +27,19 @@ fn main() {
             yoooo,
             application::commands::action_basic_information::xddddd,
         ])
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+      
+            #[cfg(target_os = "macos")]
+            apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
+              .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+      
+            #[cfg(target_os = "windows")]
+            apply_blur(&window, Some((18, 18, 18, 125)))
+              .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+      
+            Ok(())
+          })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
